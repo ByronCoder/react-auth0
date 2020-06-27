af3571586114a858e426a391387e0914c6684c0e
Byron Blank <byronbla@gmail.com>
2020-06-27 10:19:05 -0400

    added auth0 authorization with scopes, rules and roles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
-import Home from './Home';
-import Profile from './Profile';
-import Nav from './Nav';
-import Auth from './Auth/Auth';
-import Callback from './Callback';
-
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import Profile from "./Profile";
+import Nav from "./Nav";
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
+import Public from "./Public";
+import Private from "./Private";
+import Courses from "./Courses";
 
 class App extends Component {
   constructor(props) {
@@ -13,17 +15,59 @@ class App extends Component {
     this.auth = new Auth(this.props.history);
   }
   render() {
-  return <>
-    <Nav auth={this.auth} />
-    <div className="body">
-    <Route path="/" exact render={props => <Home auth={this.auth} {...props} />} />
+    return (
+      <>
+        <Nav auth={this.auth} />{" "}
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={(props) => <Home auth={this.auth} {...props} />}
+          />
 
-    <Route path="/callback" exact render={props => <Callback auth={this.auth} {...props} />} />
+          <Route
+            path="/callback"
+            exact
+            render={(props) => <Callback auth={this.auth} {...props} />}
+          />
 
-    <Route path="/profile" component={Profile} />
-    </div>
-  </>
-
+          <Route
+            path="/profile"
+            render={(props) =>
+              this.auth.isAuthenticated() ? (
+                <Profile auth={this.auth} {...props} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+          <Route path="/public" component={Public} />
+          <Route
+            path="/private"
+            exact
+            render={(props) =>
+              this.auth.isAuthenticated() ? (
+                <Private auth={this.auth} {...props} />
+              ) : (
+                this.auth.login
+              )
+            }
+          />
+          <Route
+            path="/course"
+            exact
+            render={(props) =>
+              this.auth.isAuthenticated() &&
+              this.auth.userHasScopes(["read:courses"]) ? (
+                <Courses auth={this.auth} {...props} />
+              ) : (
+                this.auth.login
+              )
+            }
+          />
+        </div>
+      </>
+    );
   }
 }
 
